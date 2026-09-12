f862d4f0931b5ec460d85391e6815633d932073e
ilyes <hammadiilyesahmed@gmail.com>
2026-09-12 16:30:26 +0200

    Update .config/nvim/lua/ilyes/plugins/treesitter.lua

--- dot_config/nvim/lua/ilyes/plugins/treesitter.lua
+++ dot_config/nvim/lua/ilyes/plugins/treesitter.lua
@@ -1,37 +1,40 @@
 -- `main` branch: parsers installed explicitly, highlighting started per
 -- filetype. `:TSUpdate` is handled by the PackChanged hook in ilyes.pack.
 local filetypes = {
-  'bash',
-  'c',
-  'diff',
-  'html',
-  'lua',
-  'luadoc',
-  'markdown',
-  'markdown_inline',
-  'query',
-  'vim',
-  'vimdoc',
-  'go',
-  'python',
-  'dockerfile',
-  'fish',
-  'yaml',
-  'helm',
-}
+	 'bash',
+	 'c',
+	 'diff',
+	 'html',
+	 'lua',
+	 'luadoc',
+	 'markdown',
+	 'markdown_inline',
+	 'query',
+	 'vim',
+	 'vimdoc',
+	 'go',
+	 'python',
+	 'dockerfile',
+	 'fish',
+	 'yaml',
+	 'helm',
+	 'javascript',
+	 'typescript',
+	 'tsx',
+ }
 
 require('nvim-treesitter').install(filetypes)
 
 require('treesitter-context').setup {
-  enable = true,
-  max_lines = 2,
-  multiline_threshold = 1,
-  trim_scope = 'inner',
-}
+	 enable = true,
+	 max_lines = 2,
+	 multiline_threshold = 1,
+	 trim_scope = 'inner',
+ }
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = filetypes,
-  callback = function()
-    vim.treesitter.start()
-  end,
-})
+	 pattern = vim.list_extend(vim.deepcopy(filetypes), { 'javascriptreact', 'typescriptreact' }),
+	 callback = function()
+		 vim.treesitter.start()
+	 end,
+ })
